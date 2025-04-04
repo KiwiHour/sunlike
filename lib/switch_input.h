2ac0432110e89cfe6a1b9f5cace5ab0d5eaebac8
KiwiHour <matthew.teabear.basford@gmail.com>
2025-04-04 21:34:54 +0100

    Fixed UP and DOWN pins being wrong, adding ability to detect holding down an input

--- lib/switch_input.h
+++ lib/switch_input.h
@@ -1,14 +1,25 @@
 #ifndef SWITCH_INPUT_H
 #define SWITCH_INPUT_H
 
-enum SwitchInput
+// TODO:
+// If i wanted to add the ability to hold down, I could make new enum values HOLDING_UP, HOLDING_DOWN, HOLDING_LEFT, HOLDING_RIGHT, HOLDING_PUSH
+// Unsure how much NONE is required for functionality later down the road, but this way holding can delegated to later controllers
+// eg. a ConfigController may want to know whats being held down, that way you can increase/decrease a value by holding rather than spamming inputs
+
+enum class SwitchInput
 {
 	NONE,
 	UP,
 	DOWN,
 	LEFT,
 	RIGHT,
-	PUSH
+	PUSH,
+
+	HOLDING_PUSH,
+	HOLDING_UP,
+	HOLDING_DOWN,
+	HOLDING_RIGHT,
+	HOLDING_LEFT
 };
 
 #endif
