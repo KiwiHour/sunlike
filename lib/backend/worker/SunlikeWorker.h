b699209a106b56838a2793935af9e8a93f2320e6
KiwiHour <matthew.teabear.basford@gmail.com>
2025-07-15 17:27:46 +0100

    removed SmartBulbAdapter from SunlikeWorker in favour for the state controller, to better ensure consistency in the app

--- lib/backend/worker/SunlikeWorker.h
+++ lib/backend/worker/SunlikeWorker.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "state/StateController.h"
 #include "core/SmartBulbAdapter.h"
 #include "state/StateController.h"
 #include "worker/directors/GenericDirector.h"
@@ -7,13 +8,9 @@
 class SunlikeWorker
 {
 private:
-	SmartBulbAdapter *bulb;
-
 	// Gets most recently started director (even if it has ended), this is used to determine the current bulb state
 	GenericDirector getMostRecentDirector();
 
 public:
-	SunlikeWorker(SmartBulbAdapter *adapter) : bulb(adapter) {}
-
 	void tick();
 };
